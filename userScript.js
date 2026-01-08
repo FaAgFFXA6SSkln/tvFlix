8403e5cdf3c1d60677ab3d7becc5a2f96de1d3a7
FaAgFFXA6SSkln <enbita@proton.me>
2026-01-09 02:01:29 +0900

    Update userScript.js

--- userScript.js
+++ userScript.js
@@ -78,11 +78,9 @@ const pathSegments = pathname.split('/').filter(seg => seg !== '');
   //요소 일괄제거
   var thisEpisodeTitle = "";
   const elementsToRemove = [
-      'div.notice', 'a.logo', '.gnb_mobile', '.top_btn', '.profile_info_ct',
-      '.ep_search', '.good', '.emer-content',  '.cast',
-      '.view-comment-area', '.over', '#bo_v_act', '#bo_vc', '#float',
-      'div.notice', 'ul.banner2', 'li.full.pc-only', 'li.full.mobile-only', '.search_title_mobile', '.category',
-      'nav.gnb.sf-js-enabled.sf-arrows', 'a.btn_login', '#bnb', '#footer', '.search_wrap ul', '.layer-footer', '.genre', '#other_list ul li p', '#footer_wrap'
+      'div.notice', 'a.logo', '.gnb_mobile', '.top_btn', '.profile_info_ct','.ep_search', '.good', '.emer-content',  '.cast','.view-comment-area', '.over', '#bo_v_act', '#bo_vc', '#float','div.notice',
+	  'ul.banner2', 'li.full.pc-only', 'li.full.mobile-only', '.search_title_mobile', '.category', 'nav.gnb.sf-js-enabled.sf-arrows', 'a.btn_login', '#bnb', '#footer', '.search_wrap ul', '.layer-footer',
+	  '.genre', '#other_list ul li p', '#footer_wrap', '.player-select'
   ];
 
   elementsToRemove.forEach(selector => {
@@ -1849,5 +1847,6 @@ const pathSegments = pathname.split('/').filter(seg => seg !== '');
   }
 
   createLayout();
+
 
 })();
